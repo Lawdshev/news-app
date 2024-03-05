5084b79c5ae4493465e03a19420581365cc5ef52
sherif <o.d.alsherif@gmail.com>
2024-03-05 07:35:08 +0100

    fix:article

--- src/app/fragments/article.tsx
+++ src/app/fragments/article.tsx
@@ -1,19 +1,34 @@
+"use client";
 import React from "react";
 import ReadMoreButton from "@/components/read-more-button";
 import { Article } from "@/helpers/types";
+import { useFavourite } from "@/app/hooks/favourite";
+import { FaHeart } from "react-icons/fa";
+import { FaRegHeart } from "react-icons/fa";
+import { useUserAuth } from "../providers/user-auth-context-provider";
 
 type Props = {
   article: Article;
 };
 
 function Article({ article }: Props) {
+    const { user } = useUserAuth();
+  const { favourite, addToFavourite, removeFromFavourite } = useFavourite();
+  
+  const handleFavourite = () => {
+    if (favourite.some((a: Article) => a.url === article.url)) {
+      removeFromFavourite(article);
+    } else {
+      addToFavourite(article);
+    }
+  }
   return (
     <article
       className="bg-slate-100
      dark:bg-slate-800 flex flex-col 
      rounded-lg shadow-lg hover:scale-105 
      hover:shadow-xl hover:bg-slate-200 
-     transition-all duration-200 ease-out"
+     transition-all duration-200 ease-out relative"
     >
       {article.urlToImage && (
         <img
@@ -34,6 +49,17 @@ function Article({ article }: Props) {
           >
             <p>{article.source.name} -</p>
             <p>{article.publishedAt}</p>
+            {
+              user && (
+                <p data-testid="favourite-button" className="cursor-pointer" onClick={handleFavourite}>
+                  {favourite.some((a: Article) => a.url === article.url) ? (
+                    <FaHeart className="cursor-pointer" size={20} />
+                  ) : (
+                    <FaRegHeart className="cursor-pointer" size={20} />
+                  )}
+                </p>
+              )
+            }
           </footer>
         </div>
         <ReadMoreButton article={article} />
